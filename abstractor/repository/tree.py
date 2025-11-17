cd243bc5e78ec2a9cfe5c7f8e91c9e7fff29b21a
narugo1992 <narugo1992@deepghs.org>
2025-11-17 16:03:21 +0800

    dev(narugo): add likes and downloads

--- abstractor/repository/tree.py
+++ abstractor/repository/tree.py
@@ -548,7 +548,7 @@ def ask_llm_for_hf_repo_info(repo_id: str, repo_type: RepoTypeTyping = 'dataset'
                 **parse_json_from_llm_output(text),
                 "repo_id": repo_id,
                 "repo_type": repo_type,
-                "downloads": repo_info.downloads_all_time,
+                "downloads": repo_info.downloads,
                 "likes": repo_info.likes,
                 "created_at": time.time(),
             }
