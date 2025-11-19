4313a81953a1eab393d5ae5ff1a823f2c9db25cd
narugo1992 <narugo1992@deepghs.org>
2025-11-19 12:03:36 +0800

    dev(narugo): x

--- abstractor/utils/truncate.py
+++ abstractor/utils/truncate.py
@@ -1,25 +1,30 @@
 import logging
-from typing import List
+from typing import List, Optional
 
 import tiktoken
 from hbutils.string import plural_word
 
 
+def _encode(text, encoder: Optional = None):
+    encoder = encoder or tiktoken.get_encoding("cl100k_base")
+    return encoder.encode(text, disallowed_special=())
+
+
 def truncated_by_token(text: str, max_tokens: int = 130000):
     encoder = tiktoken.get_encoding("cl100k_base")
-    original_length = len(encoder.encode(text))
+    original_length = len(_encode(text, encoder=encoder))
     if original_length <= max_tokens:
         return text
 
     l, r = 0, len(text)
     while l < r:
         m = (l + r + 1) // 2
-        if len(encoder.encode(text[:m])) <= max_tokens:
+        if len(_encode(text[:m], encoder=encoder)) <= max_tokens:
             l = m
         else:
             r = m - 1
 
-    logging.info(f'Text truncated {original_length!r} --> {len(encoder.encode(text[:l]))}')
+    logging.info(f'Text truncated {original_length!r} --> {len(_encode(text[:l], encoder=encoder))}')
     return text[:l]
 
 
@@ -27,7 +32,7 @@ def truncated_prompts_by_token(prompts: List[dict], max_tokens: int = 130000):
     encoder = tiktoken.get_encoding("cl100k_base")
     retval = []
     for i, prompt in enumerate(prompts):
-        length = len(encoder.encode(prompt['content']))
+        length = len(_encode(prompt['content'], encoder=encoder))
         if length <= max_tokens:
             logging.info(f'Prompt #{i}, role: {prompt["role"]!r} kept.')
             retval.append(prompt)
